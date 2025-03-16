e9feb8e36aaee8555ce1aea7249d9c2daf33c917
David Teixeira de Masin <davidteixeira.info@gmail.com>
2025-03-16 16:57:04 -0300

    Configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
+const cypressSplit = require("cypress-split");
+
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1200,
   e2e: {
-    baseUrl: 'https://cypress-playground.s3.eu-central-1.amazonaws.com/',
+    baseUrl: "https://cypress-playground.s3.eu-central-1.amazonaws.com/",
+    fixturesFolder: false,
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config);
+      return config;
+    },
   },
-  // video: true,
-  
-})
+});
